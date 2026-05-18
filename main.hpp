7473aa391808bb3e24692182b4c9ae11329379d6
Ramimami7 <rhabbal22@gmail.com>
2026-05-17 19:39:32 -0700

    main.hpp

--- main.hpp
+++ main.hpp
@@ -20,6 +20,7 @@ void printVector(vector<int>);
 
 void makeVector(vector<int> &number)
 {
+    number.clear();
     srand(time(0));              
     for (int i = 0; i < 10; i++) {
         number.push_back(rand() % 20);  
